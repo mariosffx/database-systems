e8477e607997885754b8ac3ec87b34c59b8c1b81
Marios Toparopoulos <marios@web-coders.net>
2024-10-22 22:18:35 +0300

    init: database systems

---
+++ README.md
@@ -0,0 +1,24 @@
+# EKPA Courses
+
+## Setup
+
+1. Install Docker Desktop
+2. On your terminal, run the following command:
+   ```
+   docker compose up --build --detach
+   ```
+
+## Configuration
+
+URLS:
+
+- metabase: http://localhost:9991
+- pgAdmin4: http://localhost:9993
+
+Postgres
+
+- username: postgres
+- password: password
+- database: university
+- host: localhost
+- port: 5432
--- init-db/2.0.0.1-insert-little-data.sql
+++ init-db/2.0.0.1-insert-little-data.sql
@@ -1,14 +1,3 @@
-delete from prereq;
-delete from time_slot;
-delete from advisor;
-delete from takes;
-delete from student;
-delete from teaches;
-delete from section;
-delete from instructor;
-delete from course;
-delete from department;
-delete from classroom;
 insert into classroom values ('Packard', '101', '500');
 insert into classroom values ('Painter', '514', '10');
 insert into classroom values ('Taylor', '3128', '70');
